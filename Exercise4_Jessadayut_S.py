03e29c6798c3f3e781292728668a52736859418e
jelnaja12 <jelnaja12@gmail.com>
2021-10-30 23:28:01 +0700

    Exercise4_Jessadayut_S

---
+++ Exercise4_Jessadayut_S.py
@@ -0,0 +1,14 @@
+FE=56
+GB=86
+ICS=44
+CP=98
+
+print("--- Your Score ---")
+
+print("Foundation English :",FE)
+
+print("General Business :",GB)
+
+print("Introduction to Computer Systems :",ICS)
+
+print("Computer Programming :",CP)
