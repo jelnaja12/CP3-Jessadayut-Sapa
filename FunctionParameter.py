438d1f09983c7bf4a3df95982bc257ee3e2bbf9f
jelnaja12 <jelnaja12@gmail.com>
2021-11-02 22:50:30 +0700

    Parameter

---
+++ FunctionParameter.py
@@ -0,0 +1,14 @@
+def plus(x,y):
+    print(x+y)
+def delete(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x//y)
+
+plus(10,5)
+delete(10,5)
+multiply(10,5)
+divide(10,5)
+
