ceae6979704d7d494749fa31af1baf65b23e39ff
jelnaja12 <jelnaja12@gmail.com>
2021-11-04 13:09:34 +0700

    Update ListEx1.py

--- ListEx1.py
+++ ListEx1.py
@@ -1,23 +1,20 @@
+systemMenu = {"ไก่ทอด": 35, "เป็ดทอด": 45, "ปลาทอด": 55, "ผักทอด": 20}
 menuList = []
-while True:
-    menuName = input("Please Enter Menu : ")
-    if menuName.upper() == "EXIT":
-        break
-    else:
-        price1 = 0
-        menuPrice = int(input("Price :"))
-        menuList.append([menuName,menuPrice])
-    price1 += menuPrice
+def showMenu():
+    print("ไก่ทอด 35 บาท,","เป็ดทอด 45 บาท,","ปลาทอด 55 บาท,","ผักทอด 20 บาท")
 
 def showBill():
-    print("---- My FoodS ----")
-    totalPrice = 0
+    print("---- My Food----")
+    totalMenu = 0
     for number in range(len(menuList)):
-        print(menuList[number])
-        totalPrice += price1
-    print("Total : "totalPrice)
-
-
-
-showBill()
-
+        print(menuList[number][0], menuList[number][1])
+        totalMenu += int(menuList[number][1])
+    print("Total : ",totalMenu)
+showMenu()
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuList.append([menuName, systemMenu[menuName]])
+showBill()  
